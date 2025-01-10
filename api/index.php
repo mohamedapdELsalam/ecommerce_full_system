d186883c3259330e8f6c5c8d16ce5b1ec3c37b0b
mohamedapdELsalam <63084182+mohamedapdELsalam@users.noreply.github.com>
2025-01-10 21:31:38 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $port = getenv('PORT') ?: 8000; // استخدام المنفذ من البيئة
 echo "Hello, this is your PHP API running on Vercel!";
 ?>
